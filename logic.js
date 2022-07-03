90dd6c4fe45b86f5351ae17b94698339a487319a
Thomas Lange <tigredepapier@gmx.de>
2022-07-03 13:22:35 +0200

    Improvements to Random and Display

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 let started = false;
 
+let preloaded = []
+
 async function startStopAction() {
     if (!started) {
         $('#startStop').addClass('btn-danger').removeClass('btn-success');
@@ -27,30 +29,34 @@ async function startStopAction() {
         console.error('DelayTo smaller then DelayFrom OR DelayFrom < 0')
     }
 
+    value = new RNG((new Date()).getSeconds())
+    value.setRange(valueFrom, valueTo)
+
+    delay = new RNG((new Date()).getSeconds())
+    delay.setRange(delayFrom, delayTo)
+
     started = !started;
-    await until(_ => started !== true, valueFrom, valueTo, delayFrom, delayTo);
+    preloadAudio(valueFrom, valueTo)
+    $('#config-option-elements input').prop("disabled", started);
+    await until(_ => started === true, value, delay);
 
 }
 
-function until(conditionFunction, valueFrom, valueTo, delayFrom, delayTo) {
+function until(isStarted, value, delay) {
 
     const poll = resolve => {
-        if(conditionFunction()) {
+        if(!isStarted()) {
             resolve();
         } else {
-            wait = Math.floor(Math.random() * (delayFrom - delayTo + 1) ) + delayTo;
-            num = Math.floor(Math.random() * (valueFrom - valueTo + 1) ) + valueTo;
+            wait = delay.inRange();
+            num = value.inRange();
 
             setTimeout(function() {
-                numStr = num.toString()
-                if (numStr.length < 2) {
-                    numStr = '0' + numStr;
-                }
-                console.log(numStr)
-                new Audio('https://archive.org/3/items/numbers0-100englishpronouciation/en_num_' + numStr + '.mp3').play()
                 $('#main-target').text(num)
             }, wait)
-
+            setTimeout(function() {
+                preloaded[num].play()
+            }, wait - 50)
 
             setTimeout(_ => poll(resolve), wait);
         }
@@ -59,4 +65,73 @@ function until(conditionFunction, valueFrom, valueTo, delayFrom, delayTo) {
     return new Promise(poll);
 }
 
+function numToAudioResourceString(num)
+{
+    numStr = num.toString()
+    if (numStr.length < 2) {
+        numStr = '0' + numStr;
+    }
+
+    return 'https://archive.org/3/items/numbers0-100englishpronouciation/en_num_' + numStr + '.mp3'
+}
+
+
+function preloadAudio(start, stop) {
+
+    for (let i = start; i <= stop; i++) {
+        preload(i, numToAudioResourceString(i))
+    }
+}
+
+function preload(key, source) {
+    if (preloaded[key]) return;
+    preloaded[key] = (new Audio(source))
+    preloaded[key].load()
+}
+
+/**
+ * Modified from orip
+ * From https://stackoverflow.com/a/424445
+ */
+function RNG(seed) {
+    // LCG using GCC's constants
+    this.m = 0x80000000; // 2**31;
+    this.a = 1103515245;
+    this.c = 12345;
+
+    this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+    return this
+}
+RNG.prototype.nextInt = function() {
+    this.state = (this.a * this.state + this.c) % this.m;
+    return this.state;
+}
+
+RNG.prototype.nextFloat = function() {
+    // returns in range [0,1]
+    return this.nextInt() / (this.m - 1);
+}
+
+RNG.prototype.setRange = function(from, to) {
+    this.from = from
+    this.to = to + 1
+
+}
+
+RNG.prototype.inRange = function() {
+    var rangeSize = this.to - this.from;
+    var randomUnder1 = this.nextInt() / this.m;
+    return this.from + Math.floor(randomUnder1 * rangeSize);
+}
+
+RNG.prototype.nextRange = function(start, end) {
+    // returns in range [start, end): including start, excluding end
+    // can't modulu nextInt because of weak randomness in lower bits
+    var rangeSize = end - start;
+    var randomUnder1 = this.nextInt() / this.m;
+    return start + Math.floor(randomUnder1 * rangeSize);
+}
+RNG.prototype.choice = function(array) {
+    return array[this.nextRange(0, array.length)];
+}
 
